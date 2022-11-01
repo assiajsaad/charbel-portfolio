4dd4af934e659132df32781b291c6f2fe5a7696a
assiajsaad <assia.j.saad@gmail.com>
2022-11-01 21:31:04 +0200

    Added hamburger-menu code

--- src/app/components/navbar/hamburger-menu/hamburger-menu.component.ts
+++ src/app/components/navbar/hamburger-menu/hamburger-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hamburger-menu',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HamburgerMenuComponent implements OnInit {
 
-  constructor() { }
+  isChecked: boolean = false;
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
+  clickHandler() {
+    setTimeout(() => {
+      this.isChecked = !this.isChecked;
+    }, 200);
+
+  }
+  // goToClientInfo() {
+  //   let i_id = this.client?.CLIENT_ID;
+  //   this.router.navigate(
+  //     ['/client-info'],
+  //     { queryParams: { id: i_id } }
+  //   );
+
+  // }
+
+  goToHome() {
+    this.router.navigate(
+      ['/home'],
+    );
+   }
+  goToProjects() {
+    this.router.navigate(
+      ['/projects'],
+    );
+   }
+
+  goToAboutUs() {
+    this.router.navigate(
+      ['/aboutUs'],
+    );
+  }
+
+  goToNewsEvents() {
+    this.router.navigate(
+      ['/newsEvents'],
+    );
+  }
+
+  goToContactUs() {
+    this.router.navigate(
+      ['/contactUs'],
+    );
+   }
 
 }
