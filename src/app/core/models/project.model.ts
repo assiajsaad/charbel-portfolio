cb9c47ea5f19e4c30d09b6e7424a47937dbdcb57
assiajsaad <assia.j.saad@gmail.com>
2022-12-03 15:59:00 +0200

    Edited project details component

--- src/app/core/models/project.model.ts
+++ src/app/core/models/project.model.ts
@@ -3,15 +3,35 @@ export class Project {
   name: string;
   image: string;
   description: string;
+  city: string;
+  client: string;
+  location: string;
+  buildingSurface: string;
+  buildingSite: string;
+
 
   constructor(id: number,
     name: string,
     image: string,
-    description: string) {
+    description: string,
+    city: string,
+    client: string,
+    location: string,
+    buildingSurface: string,
+    buildingSite: string,
+
+  ) {
     this.id = id;
     this.name = name;
     this.image = image;
     this.description = description;
+    this.city = city;
+    this.client = client;
+    this.location = location;
+    this.buildingSurface = buildingSurface;
+    this.buildingSite = buildingSite;
+
+
   }
 }
 
